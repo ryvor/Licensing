c818ad8e8ba872ac498b101218c6ed1ad537f862
ryvor <master_schmadoodle@outlook.com>
2023-05-26 21:58:46 +0100

    Minor changes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-// Get the query string from the current URL
-const queryString = window.location.search;
 
-// Create a new URLSearchParams object with the query string
-const params = new URLSearchParams(queryString);
+const query = new URLSearchParams(window.location.search);
 
-// Get the value of a specific parameter
-const paramValue = params.get('key');
-
-console.log(params);
+// VALIDATE KEYS
+var Validate = () => {
+	if((PROJ=query.get('project')) !== 0 && (KEY=query.get('key')) !== 0 && (UA=query.get('userAgent')) !== 0 ) {
+		console.log(PROJ);
+		console.log(KEY);
+		console.log(UA);
+	}
+}
