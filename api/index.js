93221e80038168d2f3dbc3fe8ca0962681e0e05f
ryvor <master_schmadoodle@outlook.com>
2023-05-26 22:00:10 +0100

    Minor changes

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ const query = new URLSearchParams(window.location.search);
 
 // VALIDATE KEYS
 var Validate = () => {
-	if((PROJ=query.get('project')) !== 0 && (KEY=query.get('key')) !== 0 && (UA=query.get('userAgent')) !== 0 ) {
+	var PROJ, KEY, UA;
+	if((PROJ=query.get('project')) !== 0 && (KEY=query.get('key')) !== 0 && (UA=query.get('agent')) !== 0 ) {
 		console.log(PROJ);
 		console.log(KEY);
 		console.log(UA);
